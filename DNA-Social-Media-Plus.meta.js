fa77142db10779cd6ad14d431388b99c2ba1951b
Yoga Budiman <LastRoze@users.noreply.github.com>
2025-07-31 05:22:33 +0700

    Update and rename 9GAG-Plus.meta.js to DNA-Social-Media-Plus.meta.js

---
+++ DNA-Social-Media-Plus.meta.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        DNA Social Media Plus
+// @namespace   DNA Social Media Plus
+// @version     1.0
+// @author      Last Roze
+// @description Dominion With Domination
+// @copyright   ©2020 - 2025 Yoga Budiman
+// @homepage    https://github.com/LastRoze/
+// @homepageURL https://github.com/LastRoze/
+// @website     https://lastroze.github.io/
+// @source      https://github.com/LastRoze/DNA-Social-Media-Plus
+// @icon        https://github.com/LastRoze/DNA-Social-Media-Plus/blob/master/DNA.jpg?raw=true
+// @iconURL     https://github.com/LastRoze/DNA-Social-Media-Plus/blob/master/DNA.jpg?raw=true
+// @defaulticon https://github.com/LastRoze/DNA-Social-Media-Plus/blob/master/DNA.jpg?raw=true
+// @icon64      https://github.com/LastRoze/DNA-Social-Media-Plus/blob/master/DNA.jpg?raw=true
+// @icon64URL   https://github.com/LastRoze/DNA-Social-Media-Plus/blob/master/DNA.jpg?raw=true
+// @updateURL   https://github.com/LastRoze/DNA-Social-Media-Plus/raw/master/9GAG-Plus.meta.js
+// @downloadURL https://github.com/LastRoze/DNA-Social-Media-Plus/raw/master/9GAG-Plus.user.js
+// @supportURL  https://lastroze.github.io/
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
+// @match       *://*.9gag.com/*
+// @match       *://*.instagram.com/*
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
+/* global $, jQuery */
